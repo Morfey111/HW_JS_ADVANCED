58973a0a22d39f0cf4393b80925f72399cb23984
Morfey111 <boris.marinov.marinov@gmail.com>
2022-06-27 21:00:03 +0300

    throw  err onley  run code with node

--- hw2.js
+++ hw2.js
@@ -16,55 +16,58 @@
 //     }
 // }
 
-function studentMaker(firstName, secondName, addres){
+// function studentMaker(firstName, secondName, addres){
 
-    return  {
-       firstName: firstName,
-       secondName:secondName,
-       addres: addres
-    }
-}
-
-let student1 = studentMaker('Petar', 'Petrov', {country:'Bg', town: 'Plovdiv'})
-let student2 = studentMaker('Todor', 'teodorov', {country:'UK', town: 'London'})
-
-console.log(student1)
-console.log(student2)
-
-// function studentMakerTwo(firstName, secondName, addres) {
-//   let newStudent = {};
-
-//   let _newFirstName;
-
-//   (newStudent.setNewName = -_newFirstName),
-//     (newStudent.secondName = secondName),
-//     (newStudent.addres = addres);
-
-//   return {
-//     newStudent,
-//   };
+//     return  {
+//        firstName: firstName,
+//        secondName:secondName,
+//        addres: addres
+//     }
 // }
 
-// newStudent.setNewName = function (newName) {
-//   _newFirstName = newName;
-//   console.log(`the name is changed...`);
-// };
+// let student1 = studentMaker('Petar', 'Petrov', {country:'Bg', town: 'Plovdiv'})
+// let student2 = studentMaker('Todor', 'teodorov', {country:'UK', town: 'London'})
 
-// newStudent.getName = function () {
-//   return _newFirstName;
-// };
+// console.log(student1)
+// console.log(student2)
 
-// let student1Two = studentMakerTwo("Petar", "Petrov", {
-//   country: "Bg",
-//   town: "Plovdiv",
-// });
-// let student2Two = studentMakerTwo("Todor", "teodorov", {
-//   country: "UK",
-//   town: "London",
-// });
+function studentMakerTwo(firstName, secondName, addres) {
+  let newStudent = {};
 
-// console.log(student1Two);
-// console.log(student2Two);
+  let _newFirstName;
 
-// student1Two.setNewName("haralampi");
-// student1Two.getName();
+  (newStudent.setNewName = -_newFirstName),
+    (newStudent.secondName = secondName),
+    (newStudent.addres = addres);
+
+  return {
+    newStudent,
+  };
+}
+
+newStudent.setNewName = function (newName) {
+  _newFirstName = newName;
+  console.log(`the name is changed...`);
+};
+
+newStudent.getName = function () {
+  return _newFirstName;
+};
+
+let student1Two = studentMakerTwo("Petar", "Petrov", {
+  country: "Bg",
+  town: "Plovdiv",
+});
+let student2Two = studentMakerTwo("Todor", "teodorov", {
+  country: "UK",
+  town: "London",
+});
+
+console.log(student1Two);
+console.log(student2Two);
+
+student1Two.setNewName("haralampi");
+student1Two.getName(); 
+
+
+// the err comes onley run code  with node 
